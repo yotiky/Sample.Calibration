947111ca643c849f2d4dafc0e665c34bae29f923
ywatanabe <ywatanabe@nextscape.net>
2020-07-13 11:32:50 +0900

    feat: invoke calibration sample.

---
+++ Assets/Calibration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calibration : MonoBehaviour
+{
+    public void LaunchEyeTracking()
+    {
+#if WINDOWS_UWP
+    UnityEngine.WSA.Application.InvokeOnUIThread(async () =>
+    {
+        bool result = await global::Windows.System.Launcher.LaunchUriAsync(new System.Uri("ms-hololenssetup://EyeTracking"));
+        if (!result)
+        {
+            Debug.LogError("Launching URI failed to launch.");
+        }
+    }, false);
+#else
+        Debug.LogError("Launching eye tracking not supported Windows UWP");
+#endif
+    }
+}
